07b7368c682515b93dda355570fc45f3f4dc28be
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-08-01 23:42:23 +0200

    Composing complex interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Jean on 01/08/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Jean on 01/08/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
