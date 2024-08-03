460ae42096dddd6542c36553a53b09eb0a8cf33d
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-08-03 23:53:02 +0200

    Interfacing with UIKit

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Jean on 03/08/24.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
